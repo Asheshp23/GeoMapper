1928662848529908e85db2a994a50830550ef414
ApiOSDev <attup23@gmail.com>
2024-10-23 18:27:47 -0400

    Initial Commit

---
+++ GeoMapper/GeoMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoMapperApp.swift
+//  GeoMapper
+//
+//  Created by Ashesh Patel on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct GeoMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
